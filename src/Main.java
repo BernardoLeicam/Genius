714ec056ce30a2709979f49f793322354b03d0f5
Benardo Maciel <bernardo.nunes.maciel@gmail.com>
2025-04-18 18:28:03 -0300

    Ajusta git ignore

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        for (int c = 0 ; c<2; c++) {
-            double ale = Math.random();
-            int num = (int) (1 + ale * 4);
-            if (num == 1) {
-                System.out.println("🟨");
-            } else if (num == 2) {
-                System.out.println("🟦");
-            } else if (num == 3) {
-                System.out.println("🟩");
-            } else if (num == 4) {
-                System.out.println("🟥");
-            }
-            Thered.sleep(2000);
+    public static void main(String[] args) throws InterruptedException {
 
-        }
+        boolean acertou = true;
+        int contador = 1;
+            int v[] = new int[1000000];
+            int c2 = 0;
+            for (int c = 0; c < 2; c++) {
+                // Sorteio de numero
+                double ale = Math.random();
+                int num = (int) (1 + ale * 4);
+
+                    for (int c1 = 0; c1 < 8; c1++) System.out.println();
+
+                    if (num == 1) {
+                        System.out.println("🟨");
+                    } else if (num == 2) {
+                        System.out.println("🟦");
+                    } else if (num == 3) {
+                        System.out.println("🟩");
+                    } else if (num == 4) {
+                        System.out.println("🟥");
+                    }
+
+                v[c2] = num;
+
+                Thread.sleep(2000);
+                for (int cont = 0; cont < 50; cont++) System.out.println();
+
+            }
+            c2++;
+            contador++;
+
     }
 }
 
